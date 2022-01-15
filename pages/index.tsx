cea9d2f66b52c08fb06b082f903d7b33b2cb2b6c
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-01-15 08:49:52 +0000

    add type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,34 @@
 import { client } from '../libs/client';
+import type { Article } from '../types/article';
 
-export default function Home() {
+type Props = {
+  articles: Array<Article>;
+};
+
+export default function Home({ articles }: Props) {
   return (
     <>
       <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
         記事一覧
       </h1>
       <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-        <div className="rounded overflow-hidden shadow-lg">
-          <img
-            className="w-full"
-            src="https://i.gyazo.com/ad13e228541bbaf6952f447cce456dc2.jpg"
-            alt="Sunset in the mountains"
-          />
-          <div className="px-6 py-4">テストタイトル</div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              # テストタグ
-            </span>
+        {articles.map(article => (
+          <div className="rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full"
+              src={article.eye_catch.url}
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">{article.title}</div>
+            <div className="px-6 pt-4 pb-2">
+              {article.tag && (
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                  #{article.tag}
+                </span>
+              )}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
