da777478c499183b9ef029e4a961c1fc3eba1091
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-01-16 01:29:15 +0000

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { client } from '../libs/client';
 import type { Article } from '../types/article';
+import Link from 'next/link';
 
 type Props = {
   articles: Array<Article>;
@@ -13,13 +14,17 @@ export default function Home({ articles }: Props) {
       </h1>
       <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {articles.map(article => (
-          <div className="rounded overflow-hidden shadow-lg">
+          <div className="rounded overflow-hidden shadow-lg" key={article.id}>
             <img
               className="w-full"
               src={article.eye_catch.url}
               alt="Sunset in the mountains"
             />
-            <div className="px-6 py-4">{article.title}</div>
+            <div className="px-6 py-4">
+              <Link href={`/article/${article.id}`} passHref>
+                <a>{article.title}</a>
+              </Link>
+            </div>
             <div className="px-6 pt-4 pb-2">
               {article.tag && (
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
